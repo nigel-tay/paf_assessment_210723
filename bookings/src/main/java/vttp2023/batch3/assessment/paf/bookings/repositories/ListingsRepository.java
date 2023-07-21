b28f7b7f08ea6e33dd71a975a12f01a126bf68d6
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-07-21 10:22:31 +0800

    add mongo query in repo layer

--- bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/repositories/ListingsRepository.java
+++ bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/repositories/ListingsRepository.java
@@ -1,9 +1,38 @@
 package vttp2023.batch3.assessment.paf.bookings.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ListingsRepository {
 
-	//TODO: Task 2
+	@Autowired
+	MongoTemplate mTemplate;
 
+	//TODO: Task 2
+	/**
+	 * db.getCollection('listings').aggregate(
+		[
+			{
+			$group: {
+				_id: '$address.country',
+				country: { $first: '$address.country' }
+			}
+			},
+			{ $project: { _id: 0, country: 1 } }
+		]
+		);
+	 */
+	public void getCountriesFromMongo() {
+		// continue with query and check if it returns properly
+
+
+		System.out.println();
+	}
 	
 	//TODO: Task 3
 
